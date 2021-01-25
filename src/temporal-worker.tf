1e0ff9168556c35b06efe369aba578ed25dbc944
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-01-25 12:49:18 +0100

    Upgrade to hsdp provider 0.9.1

--- src/temporal-worker.tf
+++ src/temporal-worker.tf
@@ -1,44 +1,35 @@
 resource "hsdp_container_host" "temporal_worker" {
-  count         = var.workers
-  name          = "temporal-worker-${random_id.id.hex}-${count.index}.dev"
-  volumes       = 1
-  volume_size   = var.volume_size
-  instance_type = var.worker_instance_type
+  count           = var.workers
+  name            = "temporal-worker-${random_id.id.hex}-${count.index}.dev"
+  volumes         = 1
+  volume_size     = var.volume_size
+  instance_type   = var.worker_instance_type
   security_groups = ["analytics"]
 
   user_groups = var.user_groups
 
-  connection {
-    bastion_host = var.bastion_host
-    host         = self.private_ip
-    user         = var.user
-    private_key  = var.private_key
-    script_path  = "/home/${var.user}/bootstrap.bash"
-  }
+  bastion_host = var.bastion_host
+  user         = var.user
+  private_key  = var.private_key
 
-  provisioner "remote-exec" {
-    inline = [
-      "docker volume create agent"
-    ]
-  }
+  commands = [
+    "docker volume create agent"
+  ]
 }
 
-resource "null_resource" "worker" {
+resource "hsdp_container_host_exec" "worker" {
   count = var.workers
 
   triggers = {
     cluster_instance_ids = element(hsdp_container_host.temporal_worker.*.id, count.index)
   }
 
-  connection {
-    bastion_host = var.bastion_host
-    host         = element(hsdp_container_host.temporal_worker.*.private_ip, count.index)
-    user         = var.user
-    private_key  = var.private_key
-    script_path  = "/home/${var.user}/bootstrap.bash"
-  }
+  bastion_host = var.bastion_host
+  host         = element(hsdp_container_host.temporal_worker.*.private_ip, count.index)
+  user         = var.user
+  private_key  = var.private_key
 
-  provisioner "file" {
+  file {
     content = templatefile("${path.module}/scripts/bootstrap-worker.sh.tmpl", {
       temporal_hostport   = "${hsdp_container_host.temporal.private_ip}:2181"
       docker_host         = "tcp://${element(hsdp_container_host.temporal_worker.*.private_ip, count.index)}:2375"
@@ -55,7 +46,7 @@ resource "null_resource" "worker" {
     destination = "/home/${var.user}/bootstrap-worker.sh"
   }
 
-  provisioner "file" {
+  file {
     content = templatefile("${path.module}/scripts/bootstrap-fluent-bit.sh.tmpl", {
       ingestor_host    = var.hsdp_ingestor_host
       shared_key       = var.hsdp_shared_key
@@ -67,28 +58,26 @@ resource "null_resource" "worker" {
     destination = "/home/${var.user}/bootstrap-fluent-bit.sh"
   }
 
-  provisioner "file" {
+  file {
     content     = tls_private_key.key.private_key_pem
     destination = "/home/${var.user}/private_key.pem"
   }
 
-  provisioner "file" {
+  file {
     content     = tls_self_signed_cert.server.cert_pem
     destination = "/home/${var.user}/server_cert.pem"
   }
 
-  provisioner "file" {
+  file {
     content     = tls_self_signed_cert.client.cert_pem
     destination = "/home/${var.user}/client_cert.pem"
   }
 
-  provisioner "remote-exec" {
-    # Bootstrap script called with private_ip of each node in the cluster
-    inline = [
-      "chmod +x /home/${var.user}/bootstrap-fluent-bit.sh",
-      "/home/${var.user}/bootstrap-fluent-bit.sh",
-      "chmod +x /home/${var.user}/bootstrap-worker.sh",
-      "/home/${var.user}/bootstrap-worker.sh"
-    ]
-  }
+  # Bootstrap script called with private_ip of each node in the cluster
+  commands = [
+    "chmod +x /home/${var.user}/bootstrap-fluent-bit.sh",
+    "/home/${var.user}/bootstrap-fluent-bit.sh",
+    "chmod +x /home/${var.user}/bootstrap-worker.sh",
+    "/home/${var.user}/bootstrap-worker.sh"
+  ]
 }
